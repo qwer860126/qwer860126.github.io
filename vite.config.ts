2f0166dacd30de3bfc188d97e762c60979106d1b
LAPTOP-BQCGKUDU\User <107522948+qwer860126@users.noreply.github.com>
2025-02-24 14:36:34 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "test-vite-github-pages",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
